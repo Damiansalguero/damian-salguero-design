5e7d2344683f24a109d04646e28734dd74d36df0
Dimiaun <damian.salguero@aikq.de>
2020-03-20 12:13:07 +0100

    implement handlebars

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+//For importing it into test file
+exports.app = app;
+const hb = require("express-handlebars");
+
+//Access to css/html
+app.use(express.static("./public"));
+
+//Setup handlebars
+app.engine("handlebars", hb());
+app.set("view engine", "handlebars");
+
+/////////////////////ROUTES/////////////////////
+
+//Route "Registration"//
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        layout: "main"
+    });
+});
+
+app.get("/de", (req, res) => {
+    res.render("german", {
+        layout: "main"
+    });
+});
+
+//Route "English"
+app.get("/en", (req, res) => {
+    res.redirect("/");
+});
+
+///////////NODE + HEROKU SERVER////////////////
+if (require.main == module) {
+    app.listen(process.env.PORT || 8080, () => console.log("Listening"));
+}
